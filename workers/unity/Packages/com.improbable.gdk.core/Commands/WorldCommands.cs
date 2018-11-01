d9cd23c99beeb368ff6177a483f260fa964b886a
sami <sami@improbable.io>
2018-11-01 21:45:18 +0000

    wip started command system stuff

--- workers/unity/Packages/com.improbable.gdk.core/Commands/WorldCommands.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Commands/WorldCommands.cs
@@ -13,6 +13,11 @@ namespace Improbable.Gdk.Core.Commands
 {
     public static partial class WorldCommands
     {
+        public class WorldCommandSender
+        {
+
+        }
+
         internal static void AddWorldCommandRequesters(World world, EntityManager manager, Unity.Entities.Entity entity)
         {
             var createEntitySender = new CreateEntity.CommandSender
