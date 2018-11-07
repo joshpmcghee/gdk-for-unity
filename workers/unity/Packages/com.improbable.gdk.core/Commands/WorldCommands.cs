4fe556cda03a97fbcdae3bc296d47d5a670ee4f1
sami <sami@improbable.io>
2018-11-07 16:56:20 +0000

    removed unused command storage

--- workers/unity/Packages/com.improbable.gdk.core/Commands/WorldCommands.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Commands/WorldCommands.cs
@@ -13,11 +13,6 @@ namespace Improbable.Gdk.Core.Commands
 {
     public static partial class WorldCommands
     {
-        public class WorldCommandSender
-        {
-
-        }
-
         internal static void AddWorldCommandRequesters(World world, EntityManager manager, Unity.Entities.Entity entity)
         {
             var createEntitySender = new CreateEntity.CommandSender
